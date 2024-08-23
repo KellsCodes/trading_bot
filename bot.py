bda5ccba6f7cfcdc0614288bd10a6ef73a3cb341
Ifeanyi Nworji <nworjiifeanyi@gmail.com>
2024-08-23 05:32:54 +0100

    trade account structured

--- bot.py
+++ bot.py
@@ -1,11 +1,22 @@
 # import needed libraries
 from trader_lib import *
 from logger import *
+import sys
 
 # initialize the logger
 initialize_logger()
 
 # check our trading account (blocked?, total amount?)
+
+
+def check_account_ok():
+    try:
+        # get account info
+        print("trying...")
+    except Exception as e:
+        lg.error("Could not get account info")
+        lg.info(str(e))
+        sys.exit()
 
 # close current orders
 
